c66cb201895b25406ed2b6a88cf388d9fa47a35f
WismeilyP <105970086+WismeilyP@users.noreply.github.com>
2026-01-12 22:06:53 -0400

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,22 @@
 <?php
-$host = "localhost";
+// Datos de conexión de Railway (Public Network)
+$host = "yamanote.proxy.rlwy.net";
 $user = "root";
-$pass = "";
-$db   = "marketplace";
+$pass = "JKplQkjUJESoCdgqNyHLsGzzLkfAZjme"; 
+$db   = "railway"; 
+$port = "56605";
 
-$conn = new mysqli($host, $user, $pass, $db);
+// Crear la conexión incluyendo el puerto
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+// Verificar la conexión
 if($conn->connect_error){
-    die("Conexión fallida: ".$conn->connect_error);
+    die("Conexión fallida: " . $conn->connect_error);
 }
+
+// Configuración para tildes y eñes
+$conn->set_charset("utf8");
+
+// Si quieres probar si funciona, quita las barras de la siguiente línea:
+// echo "Conexión exitosa a la nube";
 ?>
